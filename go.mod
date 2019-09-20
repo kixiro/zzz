1866823cd10db6b150b331df24ab442d0449559f
Anton Dzyk <adzyk@solit-clouds.ru>
2019-09-20 17:39:15 +0300

    dep -> mod, use pipe

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kixiro/zzz
+
+go 1.13
+
+require (
+	github.com/buger/jsonparser v0.0.0-20181115193947-bf1c66bbce23
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.9 // indirect
+)
--- main.go
+++ main.go
@@ -11,25 +11,42 @@ import (
 	"github.com/buger/jsonparser"
 )
 
+var reader *bufio.Reader
+
 func main() {
-	flag.Parse()
-	if flag.NArg() != 1 {
-		printUsage()
-		os.Exit(1)
-	}
 
-	f, err := os.Open(flag.Arg(0))
+	info, err := os.Stdin.Stat()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "LOG OPEN ERROR:", err.Error())
+		fmt.Fprintln(os.Stderr, "Not get stat stdout")
 		os.Exit(1)
 	}
-	defer f.Close()
 
-	r := bufio.NewReader(f)
+	fmt.Println(info.Mode() & os.ModeCharDevice)
+
+	if (info.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
+
+		flag.Parse()
+		if flag.NArg() != 1 {
+			printUsage()
+			os.Exit(1)
+		}
+
+		f, err := os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "LOG OPEN ERROR:", err.Error())
+			os.Exit(1)
+		}
+		defer f.Close()
+		reader = bufio.NewReader(f)
+
+	} else {
+		reader = bufio.NewReader(os.Stdin)
+	}
+
 	buf := bytes.NewBuffer(nil)
 
 	for {
-		line, pfx, err := r.ReadLine()
+		line, pfx, err := reader.ReadLine()
 		if err != nil && err != io.EOF {
 			fmt.Fprintln(os.Stderr, "LOG READ ERROR:", err.Error())
 			os.Exit(1)
@@ -44,10 +61,10 @@ func main() {
 				e := new(entry)
 				err = jsonparser.ObjectEach(buf.Bytes(), e.parse)
 				if err != nil {
-					fmt.Fprintln(os.Stderr, "LOG PARSE ERROR:", err.Error())
-					os.Exit(1)
+					fmt.Fprintln(os.Stderr, "LOG PARSE ERROR:", err.Error(), "\n", string(line))
+				} else {
+					e.print(os.Stdout)
 				}
-				e.print(os.Stdout)
 			}
 			buf.Reset()
 		}
